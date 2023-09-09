429555a9c2d17d8a3349173192be11ebe6c6cb63
Mahmoud Ahmed <mahmoudahmedibrahim5@gmail.com>
2023-09-09 21:15:44 +0300

    First 3 main functions ready (only LCD missing)

--- APP/APP.c
+++ APP/APP.c
@@ -8,3 +8,148 @@
 
 #include "APP.h"
 
+/******************* Hardware objects ******************/
+KeyPad_t input = {PORTA, {0, 1, 2, 3, 4, 5, 6, 7}};
+// LCD object
+BT_t mobile = {USART1, 9600};	// Tx PA9, Rx PA10
+/*******************************************************/
+
+
+/********************* Time Slots **********************/
+ST_Slot_t timeSlots[5];
+/*******************************************************/
+
+/****************** Constant Variables *****************/
+const u8 Password[3] = {1, 2, 3};
+/*******************************************************/
+
+void APP_voidInit(void)
+{
+	RCC_voidInit();
+
+	RCC_voidPeripheralEnable(GPIOA_EN);
+	RCC_voidPeripheralEnable(GPIOB_EN);
+	RCC_voidPeripheralEnable(USART1_EN);
+
+	BT_voidInit(&mobile);
+	// LCD initialize
+
+	for(u8 i = 0; i < 5; i++){
+		timeSlots[i].time = i;
+		timeSlots[i].availability = AVAILABLE;
+	}
+}
+
+void APP_voidStart(void)
+{
+	u8 mode;
+
+	/* On the screen following should be printed
+	 * choose the mode
+	 * 1- Admin Mode 	2- User Mode
+	 */
+
+	/* Read The input */
+	while(KEYPAD_u8Read(&input)==NO_KEY_PRESSED);
+	mode = KEYPAD_u8Read(&input);
+
+	/* Check the mode */
+	if(mode == 1)
+		APP_voidAdminMode();
+	else if(mode == 2)
+		APP_voidUserMode();
+}
+
+void APP_voidAdminMode(void)
+{
+	u8 enteredPassword[3];
+	u8 maxNumberOfTries = 3;
+
+	while(maxNumberOfTries)
+	{
+		/* On the screen following should be printed
+		 * Enter the Password:
+		 */
+
+		/* Read the entered Password */
+		for(u8 i = 0; i < 3; i++)
+		{
+			while(KEYPAD_u8Read(&input)==NO_KEY_PRESSED);
+			enteredPassword[i] = KEYPAD_u8Read(&input);
+		}
+
+		/* Check The Password */
+		u8 right = 1;
+		for(u8 i = 0; i < 3; i++)
+		{
+			if(enteredPassword[i] != Password[i])
+			{
+				right = 0;
+				break;
+			}
+		}
+
+		if(right)
+		{
+			u8 option;
+			/* On the screen following should be printed
+			 * Correct Password
+			 * delay 3 seconds
+			 * 1- Add Patient	2- Edit Patient data
+			 * 3- Reserve Slot	4- Cancel Reservation
+			 */
+			while(KEYPAD_u8Read(&input)==NO_KEY_PRESSED);
+			option = KEYPAD_u8Read(&input);
+
+			switch(option)
+			{
+			case 1:
+				APP_voidAddPatient();
+				break;
+			case 2:
+				APP_voidEditPatient();
+				break;
+			case 3:
+				APP_voidReserveSlot();
+				break;
+			case 4:
+				APP_voidCancelReservation();
+				break;
+			default:
+				// Invalid Input
+			}
+
+			break; // break from the while of number of tries
+		}
+		else
+		{
+			/* On the screen following should be printed
+			* Wrong Password
+			*/
+		}
+	}
+
+	/* This point will be reached if admin enters the password 3 times wrong */
+
+}
+
+void APP_voidUserMode(void)
+{
+	u8 option;
+
+	/* On the screen following should be printed
+	 * 1- View Patient
+	 * 2- View Reservation
+	 */
+	while(KEYPAD_u8Read(&input)==NO_KEY_PRESSED);
+	option = KEYPAD_u8Read(&input);
+
+	if(option == 1)
+		APP_voidViewPatient();
+	else if(option == 2)
+		APP_voidViewReservation();
+	else{
+		//invalid input
+	}
+}
+
--- APP/APP.h
+++ APP/APP.h
@@ -14,6 +14,61 @@
 #include "../HAL/KeyPad/KEYPAD_Interface.h"
 #include "../HAL/LED/LED_Interface.h"
 
+/*********************** Patient ***********************/
+typedef enum{
+	FEMALE,
+	MALE
+}EN_Gender_t;
 
+typedef struct{
+	u8 name[20];
+	u8 age;
+	u8 ID;
+	EN_Gender_t gender;
+}ST_Patient_t;
+
+/*******************************************************/
+
+/********************** Time Slots *********************/
+typedef enum{
+	FROM_2_TO_2_30,
+	FROM_2_30_TO_3,
+	FROM_3_TO_3_30,
+	FROM_4_TO_4_30,
+	FROM_4_30_TO_5
+}EN_SlotTime_t;
+
+typedef enum{
+	NOT_AVAILABLE,
+	AVAILABLE
+}EN_SlotAvailability_t;
+
+typedef struct{
+	EN_SlotTime_t time;
+	EN_SlotAvailability_t availability;
+	u8 ID;
+}ST_Slot_t;
+
+/*******************************************************/
+
+
+/***************** Functions Prototypes ****************/
+void APP_voidInit(void);
+
+void APP_voidStart(void);
+void APP_voidAdminMode(void);
+void APP_voidUserMode(void);
+
+// Admin mode functions
+void APP_voidAddPatient(void);
+void APP_voidEditPatient(void);
+void APP_voidReserveSlot(void);
+void APP_voidCancelReservation(void);
+
+// User mode functions
+void APP_voidViewPatient(void);
+void APP_voidViewReservation(void);
+
+/*******************************************************/
 
 #endif /* APP_H_ */
