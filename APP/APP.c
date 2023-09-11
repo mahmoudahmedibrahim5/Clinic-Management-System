9afcdaba844a6d0639822e2f1cf6919bb5b84bf5
BegadElsamadesy <135548165+BegadElsamadesy@users.noreply.github.com>
2023-09-11 23:39:50 +0300

    APP.c
    Added LCD missing functions, done with add and edit patient functions

--- APP/APP.c
+++ APP/APP.c
@@ -1,10 +1,9 @@
 /*
- * APP.c
+ * App.c
  *
- *  Created on: Sep 9, 2023
- *      Author: Mahmoud Ahmed
+ *  Created on: Sep 11, 2023
+ *      Author: dell
  */
-
 
 #include "APP.h"
 
@@ -23,6 +22,8 @@ ST_Slot_t timeSlots[5];
 const u8 Password[3] = {1, 2, 3};
 /*******************************************************/
 
+static u8 Global_PatientCounter=0;
+
 void APP_voidInit(void)
 {
 	RCC_voidInit();
@@ -32,7 +33,7 @@ void APP_voidInit(void)
 	RCC_voidPeripheralEnable(USART1_EN);
 
 	BT_voidInit(&mobile);
-	// LCD initialize
+	LCD_voidInit();
 
 	for(u8 i = 0; i < 5; i++){
 		timeSlots[i].time = i;
@@ -43,7 +44,6 @@ void APP_voidInit(void)
 void APP_voidStart(void)
 {
 	u8 mode;
-
 	/* On the screen following should be printed
 	 * choose the mode
 	 * 1- Admin Mode 	2- User Mode
@@ -71,12 +71,14 @@ void APP_voidAdminMode(void)
 		/* On the screen following should be printed
 		 * Enter the Password:
 		 */
-
+		LCD_voidClearScreen();
+		LCD_voidDisplayString("Enter Password:");
 		/* Read the entered Password */
 		for(u8 i = 0; i < 3; i++)
 		{
 			while(KEYPAD_u8Read(&input)==NO_KEY_PRESSED);
 			enteredPassword[i] = KEYPAD_u8Read(&input);
+			LCD_voidDisplayCharacter('*');
 		}
 
 		/* Check The Password */
@@ -99,6 +101,11 @@ void APP_voidAdminMode(void)
 			 * 1- Add Patient	2- Edit Patient data
 			 * 3- Reserve Slot	4- Cancel Reservation
 			 */
+			LCD_voidClearScreen();
+			LCD_voidDisplayString("Correct Password");
+			SYSTICK_voidDelayMilliSec(3);
+			LCD_voidDisplayString("1- Add Patient	2- Edit Patient data");
+			LCD_voidDisplayStringRowColumn(1,0,"3- Reserve Slot	4- Cancel Reservation");
 			while(KEYPAD_u8Read(&input)==NO_KEY_PRESSED);
 			option = KEYPAD_u8Read(&input);
 
@@ -118,6 +125,7 @@ void APP_voidAdminMode(void)
 				break;
 			default:
 				// Invalid Input
+				break;
 			}
 
 			break; // break from the while of number of tries
@@ -125,8 +133,10 @@ void APP_voidAdminMode(void)
 		else
 		{
 			/* On the screen following should be printed
-			* Wrong Password
-			*/
+			 * Wrong Password
+			 */
+			LCD_voidClearScreen();
+			LCD_voidDisplayString("Wrong Password");
 		}
 	}
 
@@ -142,6 +152,9 @@ void APP_voidUserMode(void)
 	 * 1- View Patient
 	 * 2- View Reservation
 	 */
+	LCD_voidClearScreen();
+	LCD_voidDisplayStringRowColumn(0,0,"1- View Patient");
+	LCD_voidDisplayStringRowColumn(1,0,"View Reservation");
 	while(KEYPAD_u8Read(&input)==NO_KEY_PRESSED);
 	option = KEYPAD_u8Read(&input);
 
@@ -151,6 +164,59 @@ void APP_voidUserMode(void)
 		APP_voidViewReservation();
 	else{
 		//invalid input
+		LCD_voidClearScreen();
+		LCD_voidDisplayString("invalid input");
 	}
 }
 
+void APP_voidAddPatient(void)
+{
+	LCD_voidDisplayString("Enter Patients name");
+	BT_voidReceiveData(&mobile,Patient[Global_PatientCounter].name,20);
+	SYSTICK_voidDelayMilliSec(20);
+	LCD_voidDisplayString("Enter Patients age");
+	BT_voidReceiveData(&mobile,Patient[Global_PatientCounter].age,2);
+	SYSTICK_voidDelayMilliSec(20);
+	LCD_voidDisplayString("Enter Patients gender");
+	BT_voidReceiveData(&mobile,Patient[Global_PatientCounter].gender,6);
+	SYSTICK_voidDelayMilliSec(20);
+	Patient[Global_PatientCounter].ID=Global_PatientCounter;
+	LCD_voidDisplayString(Patient[Global_PatientCounter].name);
+	LCD_voidDisplayString(" ID is ");
+	LCD_voidMoveCursor(0,14);
+	LCD_voidIntgerToString(Global_PatientCounter); //It shall display "(Patient's name) ID is (Patient's ID)"
+	SYSTICK_voidDelayMilliSec(500);
+	LCD_voidClearScreen();
+	Global_PatientCounter++;
+}
+
+void APP_voidEditPatient(void)
+{
+	u8 PatientNameToEdit[20];
+	LCD_voidDisplayString("Enter Name Of Patient To Edit");
+	BT_voidReceiveData(&mobile,PatientNameToEdit,20);
+	for(u8 i=0;i<50;i++)
+	{
+		if(PatientNameToEdit==Patient[i].name)
+		{
+			LCD_voidDisplayString("Enter Patients name");
+			BT_voidReceiveData(&mobile,Patient[Global_PatientCounter].name,20);
+			SYSTICK_voidDelayMilliSec(20);
+			LCD_voidDisplayString("Enter Patients age");
+			BT_voidReceiveData(&mobile,Patient[Global_PatientCounter].age,2);
+			SYSTICK_voidDelayMilliSec(20);
+			LCD_voidDisplayString("Enter Patients gender");
+			BT_voidReceiveData(&mobile,Patient[Global_PatientCounter].gender,6);
+			SYSTICK_voidDelayMilliSec(20);
+			Patient[Global_PatientCounter].ID=Global_PatientCounter;
+			LCD_voidDisplayString(Patient[Global_PatientCounter].name);
+			LCD_voidDisplayString(" ID is ");
+			LCD_voidMoveCursor(0,14);
+			LCD_voidIntgerToString(Global_PatientCounter); //It shall display "(Patient's name) ID is (Patient's ID)"
+			SYSTICK_voidDelayMilliSec(500);
+			LCD_voidClearScreen();
+			Global_PatientCounter++;
+			break;
+		}
+	}
+}
